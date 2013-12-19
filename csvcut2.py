969b8b148302798dcc2da8988f1887c684e354e5
Sean <hoover.sean@gmail.com>
2013-12-19 20:34:28 +0000

    started cutting columns

--- csvcut2.py
+++ csvcut2.py
@@ -6,11 +6,11 @@ import csv
 csvfile = raw_input('Enter filename with path:')
 
 #open csv file
-
+#cutting columns "csvcut -c"
 with open(csvfile, 'rb') as newfile: #examples found online didn't define 'rb' formatting option...i don't really see a difference
 	readaline = csv.reader(newfile)
 	for row in readaline:
-		print row
+		print row[0]
 
 #parse the file
 
