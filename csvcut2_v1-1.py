5d6c1f2a845e31faf36a3a6485b2cf92df6ee39b
Sean <hoover.sean@gmail.com>
2014-02-12 01:29:02 +0000

    added csvv.py using re

--- csvcut2_v1-1.py
+++ csvcut2_v1-1.py
@@ -1,15 +1,14 @@
+#calling csvcut2 >> python <script_name> <column number>
+
 import sys
 import csv 
 
-#select csv file
-
-#open csv file
 #cutting columns "csvcut -c"
 filename = sys.argv[1]
-x = sys.argv[2]
-x = int(x) - 1
+column = sys.argv[2]
+column = int(x) - 1
 with open(filename, 'rb') as newfile: #examples found online didn't define 'rb' formatting option...i don't really see a difference
 	readaline = csv.reader(newfile)
 	for row in readaline:
-		print row[x]
+		print row[column]
 
